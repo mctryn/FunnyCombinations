284405467299d79ad791c7d6fe64d53343e2c002
McTryn <mctryn@gmail.com>
2025-08-20 21:32:04 +0300

    Added GameScaffold to game screen.

--- app/src/main/java/com/mctryn/funnycombination/ui/screens/game/GameScreen.kt
+++ app/src/main/java/com/mctryn/funnycombination/ui/screens/game/GameScreen.kt
@@ -1,13 +1,10 @@
 package com.mctryn.funnycombination.ui.screens.game
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.mctryn.funnycombination.R
-import com.mctryn.funnycombination.ui.components.TopAppBar
+import com.mctryn.funnycombination.ui.components.GameScaffold
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -21,39 +18,31 @@ fun GameScreen(
     val resultOnMainMenuClicked: () -> Unit = onBackPressed
     val resultTryAgain = { gameViewModel.tryAgain() }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                stringResource(R.string.app_name),
-                true,
-                onBackPressed)
-        },
-        content = { padding ->
+    GameScaffold(showBack = true, onBackPressed = onBackPressed) { padding ->
+        when (state) {
+            is GameUIState.ShowSequenceState -> ShowSequenceScreen(
+                modifier = Modifier.padding((padding)),
+                sequence = state.sequenceList,
+                onAnimationFinished = onAnimationFinished
+            )
 
-            when (state) {
-                is GameUIState.ShowSequenceState -> ShowSequenceScreen(
-                    modifier = Modifier.padding((padding)),
-                    sequence = state.sequenceList,
-                    onAnimationFinished = onAnimationFinished
-                )
+            is GameUIState.RepeatSequenceState -> EnterSequence(
+                modifier = Modifier.padding((padding)),
+                chosenList = state.chosenList,
+                baseElements = state.baseList,
+                score = state.score,
+                onItemClicked = onItemClicked
+            )
 
-                is GameUIState.RepeatSequenceState -> EnterSequence(
-                    modifier = Modifier.padding((padding)),
-                    chosenList = state.chosenList,
-                    baseElements = state.baseList,
-                    score = state.score,
-                    onItemClicked = onItemClicked
-                )
-
-                is GameUIState.Result -> FailScreen(
-                    modifier = Modifier.padding((padding)),
-                    savedToScoreBoard = state.savedToScoreBoard,
-                    score = state.score,
-                    resultOnMainMenuClicked = resultOnMainMenuClicked,
-                    resultTryAgain = resultTryAgain
-                )
-            }
-        })
+            is GameUIState.Result -> FailScreen(
+                modifier = Modifier.padding((padding)),
+                savedToScoreBoard = state.savedToScoreBoard,
+                score = state.score,
+                resultOnMainMenuClicked = resultOnMainMenuClicked,
+                resultTryAgain = resultTryAgain
+            )
+        }
+    }
 }
 
 
